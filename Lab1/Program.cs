4d7bcf1ccd6fafbca71605a4569559a94b509f3b
Matthew J Roth <mjroth03@louisville.edu>
2020-12-02 10:59:01 -0500

    Add project files.

---
+++ Lab1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Grading ID: K1791
+//Lab 1
+//30 August 2020
+//CIS 199-02
+//Giving some information about ourselves, and writing it to the Console.
+
+using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grading ID:         K1791");
+            Console.WriteLine("Hobbies:            Hiking, playing with my dog, playing videogames, reading, bodybuilding.");
+            Console.WriteLine("Favorite Movie:     Braveheart");
+            Console.WriteLine("Favorite Book:      Hatchet");
+        }
+    }
+}
